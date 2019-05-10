5d2d2b0512ed4a75b4342b0eeedcebc33c925948
student <student@DESKTOP>
2019-05-10 09:45:17 +0900

    커밋합니다

---
+++ SafeFood_gj_2_SBH_KJY/src/main/java/com/ssafy/SafeFoodGj2SbhKjyApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SafeFoodGj2SbhKjyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SafeFoodGj2SbhKjyApplication.class, args);
+	}
+
+}
